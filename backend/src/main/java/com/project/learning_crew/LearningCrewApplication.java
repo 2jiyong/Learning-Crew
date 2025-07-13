b90bf396d7565f83e6b1086c9461a9f8c0abe69a
2jiyong <jdragon2000@naver.com>
2025-07-13 17:09:38 +0900

    chore: 스프링 설정 추가

---
+++ backend/src/main/java/com/project/learning_crew/LearningCrewApplication.java
@@ -0,0 +1,13 @@
+package com.project.learning_crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCrewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/learning_crew/LearningCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.learning_crew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
